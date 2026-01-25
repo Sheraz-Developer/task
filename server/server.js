5a63b25fc3d3a193c42904f12f7dc6aaa0636dfd
Muhammad Sheraz <asifmehmood6090@gmail.com>
2026-01-26 01:05:07 +0500

    third

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import { clerkMiddleware } from '@clerk/express'
 import { PrismaClient } from '@prisma/client';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middlewares/authMiddleware.js';
 
 const app = express();
 const prisma = new PrismaClient({
@@ -22,6 +24,9 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => res.send('Server is live!'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+//Routes
+app.use('/api/workspaces', protect, workspaceRouter);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
