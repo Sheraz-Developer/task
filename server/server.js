e269c44617391bae861e4b6a412ad52c83053c8a
Muhammad Sheraz <asifmehmood6090@gmail.com>
2026-01-25 22:55:40 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
+import { PrismaClient } from '@prisma/client';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+const app = express();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+});
+
+app.use(express.json());
+app.use(cors())
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => res.send('Server is live!'));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// Gracefully shutdown Prisma connection
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
