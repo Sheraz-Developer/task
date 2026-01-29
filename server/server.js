f1bfcdb8d03882581dbb3f18de55f3146355cece
Muhammad Sheraz <asifmehmood6090@gmail.com>
2026-01-29 21:06:43 +0500

    backend_complete

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
+import projectRouter from './routes/projectRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
+import commentRouter from './routes/commentRoutes.js';
 
 const app = express();
 const prisma = new PrismaClient({
@@ -27,6 +30,9 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //Routes
 app.use('/api/workspaces', protect, workspaceRouter);
+app.use('/api/projects', protect, projectRouter)
+app.use('/api/tasks', protect, taskRouter)
+app.use('/api/comments', protect, commentRouter)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
